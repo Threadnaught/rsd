ec96938ab34d25038995fdee6e5f176abe84bd7f
idk <patrick.hingley@live.co.uk>
2023-03-29 23:32:05 +0100

    restructure

---
+++ pysrc/setup.py
@@ -0,0 +1,17 @@
+def configure_package():
+	from numpy.distutils.misc_util import Configuration
+	from numpy.distutils.misc_util import get_info
+
+	config = Configuration('arsd')
+	config.add_extension(
+		'arsd',
+		['src/api.c', 'src/decoder.c'],
+		extra_info=get_info('npymath'),
+		libraries=['avcodec', 'avformat', 'avutil']
+	)
+
+	return config
+
+from numpy.distutils.core import setup
+
+setup(configuration=configure_package)
